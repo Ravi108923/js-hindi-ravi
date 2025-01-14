54a718dce0f37cb6b9576d03af1064e1e7836408
Ravi108923 <ravibhandare02@gmail.com>
2025-01-14 05:28:07 +0000

    container config done

--- test/index.js
+++ test/index.js
@@ -1,1 +1,19 @@
-console.log("hitesh");
+const accountId = 144553
+let accountEmail = "ravi@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2  // not allowed //{} called as scope
+
+/*
+Prefered not to used var 
+Because of issue in Block scope and Functional scope
+*/
+accountEmail = "hc@gmail.com";
+accountPassword = "234567";
+accountCity = "nagpur";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
